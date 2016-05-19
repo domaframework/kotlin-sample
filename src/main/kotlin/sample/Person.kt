5d250d71ca330134199e1bd1b4d72375e1657bd9
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2016-05-19 09:06:00 +0900

    `@ParameterName` を除去

--- src/main/kotlin/sample/Person.kt
+++ src/main/kotlin/sample/Person.kt
@@ -3,12 +3,11 @@ package sample
 import org.seasar.doma.Entity
 import org.seasar.doma.GeneratedValue
 import org.seasar.doma.Id
-import org.seasar.doma.ParameterName
 
 @Entity(immutable = true)
 data class Person(
         @Id
         @GeneratedValue(strategy = org.seasar.doma.GenerationType.IDENTITY)
-        @ParameterName("id") val id:Int? = null,
-        @ParameterName("name") val name:Name,
-        @ParameterName("age") val age:Int?)
+        val id:Int? = null,
+        val name:Name,
+        val age:Int?)
