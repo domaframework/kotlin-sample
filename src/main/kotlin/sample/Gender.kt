9a9b30aefa79a9a4e8b06a09d97e3373425d786a
kogayushi <youshe.old.pleasure@gmail.com>
2019-11-17 10:35:51 +0900

    add comment for example of enum without additional value

--- src/main/kotlin/sample/Gender.kt
+++ src/main/kotlin/sample/Gender.kt
@@ -3,6 +3,8 @@ package sample
 import org.seasar.doma.Domain
 
 @Domain(valueType = String::class, accessorMethod = "getCode", factoryMethod = "of")
+// if you use enum without additional value and also do not use its value for database, you can do like this.
+//@Domain(valueType = String::class, accessorMethod = "name", factoryMethod = "valueOf")
 enum class Gender(val code:String) {
     MALE("0"),
     FEMALE("1");
