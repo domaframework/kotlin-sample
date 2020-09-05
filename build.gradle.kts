1f0d0aeded509e851c127a6155bd6851ed6705c9
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2020-09-05 12:53:36 +0900

    Remove kotlin-stdlib-jdk8

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,6 @@ application {
 dependencies {
     kapt("org.seasar.doma:doma-processor:2.41.0")
     implementation("org.seasar.doma:doma-core:2.41.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2:1.3.175")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
