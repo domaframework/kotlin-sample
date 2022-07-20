3021b96ddb5089c48e925a4d814e96ac81b88700
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2022-07-20 21:31:59 +0900

    Update doma-compile-plugin to 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     id("application")
     id("com.diffplug.spotless") version "6.7.2"
     id("org.seasar.doma.codegen") version "1.4.1"
-    id("org.seasar.doma.compile") version "1.1.0"
+    id("org.domaframework.doma.compile") version "2.0.0"
     kotlin("jvm") version "1.6.21"
     kotlin("kapt") version "1.6.21"
 }
