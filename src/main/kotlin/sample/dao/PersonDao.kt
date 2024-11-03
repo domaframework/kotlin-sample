ea1686b652937a7b15c83e5e020a82fd9578b851
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2024-11-04 08:27:17 +0900

    Replace KEntityql with KQueryDsl

--- src/main/kotlin/sample/dao/PersonDao.kt
+++ src/main/kotlin/sample/dao/PersonDao.kt
@@ -4,7 +4,7 @@ import org.seasar.doma.Dao
 import org.seasar.doma.Select
 import org.seasar.doma.Sql
 import org.seasar.doma.jdbc.Config
-import org.seasar.doma.kotlin.jdbc.criteria.KEntityql
+import org.seasar.doma.kotlin.jdbc.criteria.KQueryDsl
 import sample.domain.Name
 import sample.entity.Department_
 import sample.entity.Person
@@ -12,8 +12,8 @@ import sample.entity.Person_
 
 @Dao
 interface PersonDao {
-    private val entityql
-        get() = KEntityql(Config.get(this))
+    private val queryDsl
+        get() = KQueryDsl(Config.get(this))
 
     @Sql("select * from person where id = /*id*/0")
     @Select
@@ -27,7 +27,7 @@ interface PersonDao {
         val p = Person_()
         val d = Department_()
 
-        return entityql.from(p).innerJoin(d) {
+        return queryDsl.from(p).innerJoin(d) {
             eq(p.departmentId, d.id)
         }.where {
             eq(d.name, Name(departmentName))
@@ -38,19 +38,19 @@ interface PersonDao {
 
     fun insert(person: Person): Person {
         val p = Person_()
-        val result = entityql.insert(p, person).execute()
+        val result = queryDsl.insert(p).single(person).execute()
         return result.entity
     }
 
     fun update(person: Person): Person {
         val p = Person_()
-        val result = entityql.update(p, person).execute()
+        val result = queryDsl.update(p).single(person).execute()
         return result.entity
     }
 
     fun delete(person: Person): Person {
         val p = Person_()
-        val result = entityql.delete(p, person).execute()
+        val result = queryDsl.delete(p).single(person).execute()
         return result.entity
     }
 }
